355d05eb85ecab79cca5e345cb0de5d833ec812f
LEEJEHEON <dlwpgjs0723@gmail.com>
2021-06-26 23:09:23 +0900

    2021_06_26_java

---
+++ java_practice/src/baekjoon/baekjoon_2420.java
@@ -0,0 +1,12 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon_2420 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		System.out.print(Math.abs(a-b));
+	}
+}
+
