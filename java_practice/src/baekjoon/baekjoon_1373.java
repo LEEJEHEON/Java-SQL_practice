0302e4fdfe91d1d270fbcc765e5c9ea35362404e
LEEJEHEON <dlwpgjs0723@gmail.com>
2021-07-15 21:27:51 +0900

    2021_07_15_java_미풀이

--- java_practice/src/baekjoon/baekjoon_1373.java
+++ java_practice/src/baekjoon/baekjoon_1373.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 public class baekjoon_1373 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String a [] = sc.next().split("");
-		int k = a.length-1;
-		String answer ="";
-		while(k > 0) {
+		String a= sc.next();
+		boolean temp = true;
+		int v = a.length()-3;
+		int w = a.length();
+		StringBuilder answer = new StringBuilder();
+		String[] b ;
+		int sum=0;
+		while(temp) {
 			int i =0;
-			int sum=0;
-			while (i<3) {
-				if (k < 0)
-					break;
-				if (Integer.parseInt(a[k])==1)
+			sum=0;
+			if (v>0) {
+				b= a.substring(v,w).split("");
+				w= v;
+				v -=3;
+			}
+			else {
+				b = a.substring(0,w).split("");
+				temp = false;
+			}
+			for (int p=b.length-1;p>=0;p--) {
+				System.out.println(b[p]+"  "+i+" "+sum);
+				if (Integer.parseInt(b[p])==1)
 					sum += (int) Math.pow(2,i);
 				i++;
-				k--;
-			}
+				}
 			System.out.println("sum : "+sum);
-			answer = Integer.toString(sum) + answer;
+			answer.append(Integer.toString(sum));
 		}
-		if (answer.equals(""))
+		if (a.equals(0))
 			System.out.print(0);
 		else
-			System.out.print(answer);
+			System.out.print(answer.reverse());
 	}
 }
-// 메모리 초과 오류뜸 전체 split 하지말고 세자리씩 나눠서 split 하는 방향으로 코딩
