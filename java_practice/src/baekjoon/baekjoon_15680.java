21740c0006de95fcee8b5c70e305195c8237adee
LEEJEHEON <dlwpgjs0723@gmail.com>
2021-07-16 22:35:47 +0900

    2021_07_16_java

---
+++ java_practice/src/baekjoon/baekjoon_15680.java
@@ -0,0 +1,13 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon_15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if (N==0)
+			System.out.print("YONSEI");
+		else
+			System.out.print("Leading the Way to the Future");
+	}
+}
