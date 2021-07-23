38b019d791a80c5044a781b9893902aa12a62e74
LEEJEHEON <dlwpgjs0723@gmail.com>
2021-07-23 21:21:24 +0900

    2021_07_23_java

---
+++ java_practice/src/baekjoon/baekjoon_11719.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon_11719 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for(int i=0;i<100;i++)
+			System.out.print(sc.nextLine());
+	}
+}
