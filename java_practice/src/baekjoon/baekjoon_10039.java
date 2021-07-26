7b84ddeab04b03217a2053908837b6f4835a1f06
LEEJEHEON <dlwpgjs0723@gmail.com>
2021-07-26 20:18:24 +0900

    2021_07_26_java

---
+++ java_practice/src/baekjoon/baekjoon_10039.java
@@ -0,0 +1,17 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon_10039 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		for(int i=0;i<5;i++) {
+			int a = sc.nextInt();
+			if(a<40)
+				sum += 40;
+			else
+				sum +=a;
+		}
+		System.out.print(sum/5);
+	}
+}
